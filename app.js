bf0787fe9ce3e6c0fbd66e0941d4a81780f5fbed
Muhammed Yavuz Yeleğen <87283177+yavuzyelegen@users.noreply.github.com>
2024-06-20 21:35:46 +0300

    HTML CSS JS Final notu hesaplama web uygulaması
    vize1, vize2, final notu, final katsayısı girip final notu aldığımız bir web uygulaması.

---
+++ app.js
@@ -0,0 +1,16 @@
+/*let finalKatsayi=60;
+let vize1
+let vize2
+let final*/
+
+function myFunction(event){
+    event.preventDefault();
+    const vize1 = parseFloat(document.getElementById("vize1").value);
+    const vize2 = parseFloat(document.getElementById("vize2").value);
+    const final = parseFloat(document.getElementById("final").value);
+    const finalKatsayi = parseFloat(document.getElementById("finalKatsayi").value);
+    
+    const finalNotu = final*finalKatsayi/100+(vize1+vize2)/2*(100-finalKatsayi)/100;
+    
+    document.getElementById("sonuc").innerHTML="final notunuz "+finalNotu.toFixed(2);
+}
